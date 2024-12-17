80225cf555f76bd5de13fbd457e5180432b81140
Simoncarlosv <simoncarlosv@gmail.com>
2024-12-17 11:25:41 -0300

    Añadí explicación de por qué la ruta de filtro debe ir antes de las que van con :id como recordatorio

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,13 @@ const { GetAllJoyasController,
 
 const router = Router()
 
-router.get('/joyas/filtered',  GetJoyasFiltroController)
+//Rutas
+router.get('/joyas/filtered',  GetJoyasFiltroController) // Ruta de FILTRO
+// EXPLICACION DE POR QUE FILTRO VA DE LOS PRIMEROS
+// Cuando se coloca la ruta del filtro al final o después de las rutas con :ID
+// La aplicacion se confunde y toma como ID la segunda parte de la ruta
+// es decir, /filtered, entonces arroja un error al hacer una consulta
+// donde el error menciona que no se encuentra la columna "filtered"
 router.get('/joyas', GetAllJoyasController)// para HATEOS
 router.get('/joyas/:id', GetSingleJoyasController)// para UNA JOYA por ID
 
